627210697220d619e0b057ac6bf04585e7ce2a6d
SorooshDeveloper <sorooshonlineschool@gmail.com>
2024-04-14 16:25:46 +0330

    updated login mappings.

--- apps/ui/src/app/utils/constants.ts
+++ apps/ui/src/app/utils/constants.ts
@@ -2,7 +2,13 @@ export const USDT = '0xD434D9f8293Be6c473FA55D1103103b83197225E';
 export const USDC = '0x430363F1C8D4B2F366f67cc8C010Bb0d1DDC4f22';
 export const PRESALE_CONTRACT_ADDRESS = '0x600ed659fDf39a6ceB6098BfC403F2147094A3b0';
 export const loginMapping: { [key: string]: number } = {
-  test1: 1,
-  test2: 2,
-  test3: 3,
+  DIPSI: 1,
+  JELLY: 2,
+  ARI: 3,
+  RAFI: 4,
+  AMIR: 5,
+  NATI: 6,
+  RAY: 7,
+  METIN: 8,
+  PRESALEROUND001: 9,
 };
