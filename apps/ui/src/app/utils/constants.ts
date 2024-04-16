68ae7355725e1613c9818b46b1eac974d11d1e56
SorooshDeveloper <sorooshonlineschool@gmail.com>
2024-04-16 09:20:33 +0330

    updated contract address

--- apps/ui/src/app/utils/constants.ts
+++ apps/ui/src/app/utils/constants.ts
@@ -1,5 +1,5 @@
-export const USDT = '0xbF83202eD252003D7a3B29ab650Ce750BD8154E7';
-export const USDC = '0xbF83202eD252003D7a3B29ab650Ce750BD8154E7';
+export const USDT = '0x3d0a6583af16776f10CabE85eac457539F8e2979';
+export const USDC = '0x1a111B6Ab998D51ed0354fb70922d967730720D2';
 export const PRESALE_CONTRACT_ADDRESS = '0xbF83202eD252003D7a3B29ab650Ce750BD8154E7';
 export const loginMapping: { [key: string]: number } = {
   DIPSI: 1,
